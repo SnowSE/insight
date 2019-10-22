824c724040bb7998bd2ead8a388d259fd5debdbc
Alex Mickelson <alexmickelson96@gmail.com>
2019-10-21 21:38:06 -0600

    working on cognetive script

--- installazurecli.ps1
+++ installazurecli.ps1
@@ -1,3 +1,28 @@
+# https://docs.microsoft.com/en-us/azure/virtual-machines/scripts/virtual-machines-linux-powershell-sample-create-vm?toc=%2fazure%2fvirtual-machines%2flinux%2ftoc.json
+# https://mcpmag.com/articles/2019/05/14/azure-cognitive-service-accounts-powershell.aspx
+
 # Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; 
 # Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+
+#to install
 Install-Module -Name Az -AllowClobber -Scope CurrentUser
+
+# Variables for common values
+$resourceGroup = "myResourceGroup"
+$location = "westeurope"
+$vmName = "myVM"
+
+
+
+Connect-AzAccount
+
+New-AzResourceGroup \
+    -Name $resourceGroup \
+    -Location $location
+
+New-AzCognitiveServicesAccount \
+    -ResourceGroupName $resourceGroup \
+    -Name TestAccount \
+    -Type SpeechServices \
+    -SkuName F0 \
+    -Location $location
